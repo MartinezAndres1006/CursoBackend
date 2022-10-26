c4bc4743b18a313ead1d2b494e452ba86cbddff0
Andres Martinez <amartinezcarrillo1006@gmail.com>
2022-10-26 12:05:36 -0300

    Adelantando algo

---
+++ contenedor.js
@@ -0,0 +1,102 @@
+const fs= require('fs')
+
+class Contenedor{
+   
+   
+    save(product, file) {
+        console.log("Guardando...", product);
+
+        let nextId = this.getNextId(file);
+
+        product.id = nextId;
+
+        const allProductsArray = this.read(file);
+
+        allProductsArray.push(product);
+
+        this.write(allProductsArray, file);
+      }
+    
+      getNextId(file) {
+        let lastId = 0;
+        let allProductsArray = this.read(file);
+        if (allProductsArray.length > 0) {
+          lastId = allProductsArray[allProductsArray.length - 1].id;
+        }
+        return lastId + 1;
+      }
+    
+      read(file) {
+        let allProductsArray = [];
+        try {
+          let allProductsString = fs.readFileSync(file, "utf8");
+          
+          allProductsString.length > 0
+            ? (allProductsArray = JSON.parse(allProductsString))
+            : (allProductsArray = []);
+        } catch (err) {
+          console.log("Error en la lectura del archivo", err);
+        }
+        return allProductsArray;
+      }
+    
+      async write(allProductsArray, file) {
+       
+        let allProductsString = JSON.stringify(allProductsArray);
+        try {
+          await fs.writeFileSync(file, allProductsString);
+        } catch (err) {
+          console.log("Error en la escritura", err);
+        }
+      }
+    getAll(productos){
+        let Allproducts= this.read(productos)
+        return Allproducts
+    }
+    getById(id,productos){
+        let Allproducts= this.read(productos)
+        let producto=Allproducts.find((producto)=>producto.id==id)
+        return producto
+      }
+    deleteAll(file){
+      let Allproducts= this.read(file)
+          if(Allproducts>=0){
+            try{
+              fs.unlinkSync(Allproducts)
+            }catch(error){
+              console.log("No se pudo eliminar el archivo deseado",error)}
+          }else{
+            console.log("No existe ningun archivo")
+          }
+    }
+    deleteById(id,productos){
+      let Allproducts= this.read(productos)
+      let producto=Allproducts.find((producto)=>producto.id==id)
+          if(producto>=0){
+            producto.splice(producto,3)
+          }
+      let parseo = JSON.stringify(allProductsArray);
+      try{
+        fs.writeFileSync(productos,parseo)
+      }catch(error){
+        console.log("No se pudo eliminar el archivo deseado",error)
+      }
+      return producto
+    }
+
+    async createFile(file_path) {
+      try {
+
+        await fs.promises.writeFile(file_path, "", "utf8");
+
+      } 
+      catch (err) {
+  
+        console.log("Error en la creación del archivo", err);
+  
+      }
+    }
+  }
+    
+
+module.exports=Contenedor
---
+++ index.js
@@ -0,0 +1,56 @@
+const Contenedor= require('./contenedor.js')
+
+const contenedor = new Contenedor();
+const file= './products.txt'
+let productosLista= [{
+	nombre:"casio",
+    modelo:"fx-95",
+    id:1
+},{
+	nombre:"calculadora",
+    modelo:"fx-570",
+    id:2
+},{
+	nombre:"kemeii",
+    modelo:"fx-95",
+    id:3
+}]
+
+function test(){
+let tiendita= contenedor.createFile("./products.txt")
+
+const archivoDeTienda = contenedor.createFile(file);
+tiendita ? guardarproductos(): console.log('No se pudo guardar productos');
+tiendita ? obtenerProductos() : console.log('No se pudo leer productos');
+const productExist = archivoDeTienda? obtenerporId(1) :console.log("No hay nada");
+productExist? eliminarPorid(1) :console.log("No hay nada");
+// termina();
+
+function guardarproductos(){
+    productosLista.map(product=>{contenedor.save(product,file)})
+}
+function obtenerProductos(){
+    let loquehay =contenedor.read(file)
+    console.log(loquehay)
+}
+function obtenerporId(id){
+let obtener= contenedor.read(id)
+if(obtener){
+    console.log(obtener)
+}else{
+    console.log("Mi loco dele pa afuera que aqui no hay ná")
+}
+
+
+}
+function eliminarPorid(id){
+    let clear=contenedor.deleteById(id,file)
+        console.log(clear)
+}
+
+// function termina(){
+// contenedor.deleteAll(file)
+// console.log("Sigue corriendo...")
+// }
+}
+test()
